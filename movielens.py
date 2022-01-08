6495f86da629d6abde0f4ef1dd7c699d2863c384
poldi2018 <greenion@gmx.de>
2022-01-08 21:35:00 +0100

    implemented question 4

--- movielens.py
+++ movielens.py
@@ -10,32 +10,33 @@ dic = df.to_dict()
 # get the column names
 # keys: userId, movieId, rating, timestamp, title, genres
 object_list = pyreadr.list_objects('./data/validation.rds')
-columns = object_list[0]['columns']
+columns = list(dic.keys())
 
 def questions():
     # question 1
     # rowcount is decreased by 1 for the indices
     rows = 0
     for row in dic["title"]:
-        rows = rows+1
+        rows += 1
 
-    print('1. How many rows and columns are there in the dataset?')
+
+    print('The movielens quizz! \r\n \r\n1. How many rows and columns are there in the dataset?')
     print('Answer: There are', rows-1, 'rows and', len(dic.keys()), 'columns in the dataset. \r\n')
 
     # question 2a
-    rating_zeros=0
+    rating_zeros = 0
     for value in dic["rating"].values():
-        if value==0.0:
-            rating_zeros+=1
+        if value == 0.0:
+            rating_zeros += 1
 
     print('How many zeros were given as ratings in the dataset?')
     print('Answer: ', rating_zeros, 'movies rated with 0 \r\n')
 
     # question 2b
-    rating_threes=0
+    rating_threes = 0
     for value in dic["rating"].values():
-        if value==3.0:
-            rating_threes+=1
+        if value == 3.0:
+            rating_threes += 1
     print('How many threes were given as ratings in the dataset?')
     print('Answer: ', rating_threes, 'movies rated with 3 \r\n')
 
@@ -43,10 +44,18 @@ def questions():
     # question 3
     movies = 0
     for movie in dic["movieId"]:
-        movies+=1
+        movies += 1
     print('How many different movies are there in the dataset?')
     print('Answer: There are', movies-1, 'movies in the dataset. \r\n')
 
+
+    # question 4
+    usercount = 0
+    for user in dic["userId"].values():
+        if user > usercount:
+            usercount = user
+    print('How many different users are there in the dataset?')
+    print('Answer: There are', usercount, 'different users in the dataset. \r\n')
 
 
 
