e599b16fced43c798032b9e9b377e21c4315c8f7
poldi2018 <greenion@gmx.de>
2022-01-09 20:59:48 +0100

    implemented question 8

--- movielens.py
+++ movielens.py
@@ -1,14 +1,9 @@
 import pyreadr
-import json
 
-## result = pyreadr.read_r('./data/dataset.rds')
+# result = pyreadr.read_r('./data/dataset.rds')
 result = pyreadr.read_r('./data/validation.rds')
 df=result[None]
 dic = df.to_dict()
-# conversion of dataframe to dictionary
-# with open('test.json', 'r') as f:
-#     content = json.load(f)
-#     dic = dict(content)
 # get column names
 # keys: userId, movieId, rating, timestamp, title, genres
 columns = list(dic.keys())
@@ -77,36 +72,52 @@ def most_ratings(dict):
     movies_to_check.update(winner)
     return movies_to_check
 
+def common_ratings():
+    halfstar = 0
+    fullstar = 0
+    result = ''
+    for rating in dic['rating'].values():
+        if rating % 1 == 0.5:
+            halfstar+=1
+        elif rating % 1 == 0.0:
+            fullstar+=1
+    if halfstar > fullstar:
+        result='halfstars are more common: '+ str(halfstar)+' halfstars vs '+str(fullstar)+' fullstars. So it is false.' 
+    else:
+        result='fullstars are more common: '+ str(fullstar)+' fullstars vs '+str(halfstar)+' halfstars. So it is true.'
+    return result 
+
+
 def questions():
     # question 1
     rows=get_rows()
-    print('The movielens quizz! \r\n \r\n1. How many rows and columns are there in the dataset?')
+    print('\r\n             *** The movielens quizz! *** \r\n \r\nQuestion 1: How many rows and columns are there in the dataset?')
     print('Answer: There are', rows, 'rows and', len(columns), 'columns in the dataset. \r\n')
 
     # question 2a
     rated_zeros = ratecount(0.0)
-    print('How many zeros were given as ratings in the dataset?')
+    print('Question 2a: How many zeros were given as ratings in the dataset?')
     print('Answer: ', rated_zeros, 'movies rated with 0 \r\n')
 
     # question 2b
     rated_threes = ratecount(3.0)
-    print('How many threes were given as ratings in the dataset?')
+    print('Question 2b: How many threes were given as ratings in the dataset?')
     print('Answer: ', rated_threes, 'movies rated with 3 \r\n')
 
     # question 3
     movie_count = moviecount()
-    print('How many different movies are there in the dataset?')
+    print('Question 3: How many different movies are there in the dataset?')
     print('Answer: There are', movie_count, 'movies in the dataset. \r\n')
 
     # question 4
     usercount = get_users()
-    print('How many different users are there in the dataset?')
+    print('Question 4: How many different users are there in the dataset?')
     print('Answer: There are', usercount, 'different users in the dataset. \r\n')
 
     # question 5
     genres = {'Drama': 0, 'Comedy': 0, 'Thriller': 0, 'Romance': 0}
     returned_ratings = ratings_genre(genres)
-    print('How many movie ratings are in each of the following genres in the dataset?')
+    print('Question 5: How many movie ratings are in each of the following genres in the dataset?')
     print('Drama: ', returned_ratings['Drama'], 'ratings |', 'Comedy: ',returned_ratings['Comedy'], 'ratings |')
     print( 'Thriller: ', returned_ratings['Thriller'], 'ratings |', 'Romance: ', returned_ratings['Romance'], 'ratings |')
 
@@ -115,14 +126,18 @@ def questions():
                        'Pulp Fiction': 0, 'Shawshank Redemption': 0,
                        'Speed 2: Cruise Control': 0}
     checked_movies = most_ratings(movies_to_check)
-    print('Which movie has the greatest number of ratings?')
+    print('Question 6: Which movie has the greatest number of ratings? \r\n')
     print('Forrest Gump: ', checked_movies['Forrest Gump'], 'ratings |',
           'Jurassic Park (1993) ',checked_movies['Jurassic Park (1993)'],
           'ratings |')
     print('Pulp Fiction: ', checked_movies['Pulp Fiction'], 'ratings |',
           'The Shawshank Redemption: ', checked_movies['Shawshank Redemption'], 'ratings |')
-    print('Speed 2: Cruise Control: ', checked_movies['Speed 2: Cruise Control'], 'ratings |')
-    
-    print('THE WINNER IS:', checked_movies['winner']['title'], 'with', checked_movies['winner']['ratings'], 'ratings')
+    print('Speed 2: Cruise Control: ', checked_movies['Speed 2: Cruise Control'], 'ratings | \r\n')
+
+    print('THE WINNER IS:', checked_movies['winner']['title'], 'with', checked_movies['winner']['ratings'], 'ratings \r\n')
+
+    # question 8
+    print('Question 8: True or False: In general, half star ratings are less common than whole star ratings.')
+    print('Answer:', common_ratings(), '\r\n')
 
 questions()
